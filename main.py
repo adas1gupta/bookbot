59a43bfa58692417c7dedc1f5bc39db2de04f01b
Arjun Dasgupta <dasgupta1arjun@gmail.com>
2025-02-10 13:17:16 -0500

    finished book report printing functionality

--- main.py
+++ main.py
@@ -2,7 +2,15 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-    print(char_count(file_contents))
+    dictionary = char_count(file_contents)
+    print("--- Begin report of books/frankenstein.txt ---")
+    num_words = word_count(file_contents)
+    print(f"{num_words} words found in the document \n")
+    for char in dictionary:
+
+        if char.isalpha() is True:
+            print(f"The \'{char}\' character was found {dictionary[char]} times")
+    print("--- End report ---")
 
 def word_count(file):
     return len(file.split())
