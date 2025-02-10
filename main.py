23a7a46eb5c8c6c52e764d82823c94d1b9e95dc8
Arjun Dasgupta <dasgupta1arjun@gmail.com>
2025-02-10 13:10:48 -0500

    char count function added

--- main.py
+++ main.py
@@ -2,9 +2,19 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-    print(word_count(file_contents))
+    print(char_count(file_contents))
 
 def word_count(file):
     return len(file.split())
+
+def char_count(file):
+    dictionary = {}
+    for char in file:
+        char_lower = char.lower()
+        if char_lower not in dictionary:
+            dictionary[char_lower] = 1
+        else:
+            dictionary[char_lower] += 1
+    return dictionary
 
 main()
